04e45793609afa34daeeb05b3270bc531b52d8df
Ezra Serebrin <ezraserebrin@gmail.com>
2020-11-24 00:11:04 -0600

    Earth and Sun

--- src/main/scala/UniverseVisualizer.scala
+++ src/main/scala/UniverseVisualizer.scala
@@ -1,24 +1,35 @@
-import scalafx.application.JFXApp
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.scene.Scene
 import scalafx.scene.canvas.Canvas
 import scalafx.animation.AnimationTimer
 import scalafx.scene.paint.Color
+import scalafx.scene.input.KeyEvent
+import scalafx.scene.input.KeyCode
+import scala.collection.mutable
 
 object UniverseVisualizer extends JFXApp {
-    val displayWidth = 500
-    val displayHeight = 500
+    val displayWidth = 1280
+    val displayHeight = 720
 
     stage = new JFXApp.PrimaryStage {
-        title = "Universe Visualization"
+        title = "If the Sun was 1 pixel"
         scene = new Scene(displayWidth, displayHeight) {
             val canvas = new Canvas(displayWidth, displayHeight)
             content = canvas
             val g = canvas.graphicsContext2D
             val args = parameters.raw
 
-            val ship = new Spaceship(displayWidth / 2 - 15, displayHeight - 100)
+            var keysPressed = mutable.Set[KeyCode]()
+            canvas.onKeyPressed = (e: KeyEvent) => keysPressed += e.code
+            canvas.onKeyReleased = (e: KeyEvent) => keysPressed -= e.code
+
+            var scale = 109.0
+
+            val sun = new Star(1.0)
+            val earth = new Earth()
+            var objects = mutable.Buffer(sun, earth)
+            // val betelgeuse = new Star(sunRadius * 764)
 
             var oldT = 0L
             var timer = 0L
@@ -27,10 +38,16 @@ object UniverseVisualizer extends JFXApp {
 
                     g.setFill(Color.Black)
                     g.fillRect(0, 0, displayWidth, displayHeight)
-                    ship.display(g)
 
-                    if (timer % 60 == 0) ship.step()
-                    timer += 1
+                    earth.draw(g, scale, 20, displayHeight / 2)
+                    sun.draw(g, scale, 150, 100)
+
+                    if (keysPressed.contains(KeyCode.Down) && scale > 1.0) scale -= 1.0
+                    if (keysPressed.contains(KeyCode.Up)) scale += 1.0
+
+                    if (keysPressed.contains(KeyCode.Right)) objects.foreach(_.moveRight())
+                    if (keysPressed.contains(KeyCode.Left)) objects.foreach(_.moveLeft())
+                    // betelgeuse.draw(g, 0, 0)
 
                     oldT = t
                 }
