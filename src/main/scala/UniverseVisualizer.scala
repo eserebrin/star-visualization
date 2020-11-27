454c584ce6c4c85a290511339343ab1ead8fd343
Ezra Serebrin <ezraserebrin@gmail.com>
2020-11-27 15:27:29 -0600

    more stars and captions

--- src/main/scala/UniverseVisualizer.scala
+++ src/main/scala/UniverseVisualizer.scala
@@ -12,13 +12,17 @@ object UniverseVisualizer extends JFXApp {
     val displayWidth = 1280
     val displayHeight = 720
 
-    def caption(x: Long, scale: Double): String = {
-        if (x <= 1000) "This is Earth. Press right to see the Sun"
+    def caption(x: Long): String = {
+        if      (x <= 1000) "This is Earth. Press right to see the Sun"
         else if (x <= 1150) "The sun is huge in comparison to the small blue dot that is our home."
         else if (x <= 1500) ""
-        else if (x <= 1700 && scale > 1.0) "Press down to zoom out and see the Sun as just one pixel."
-        else if (x <= 1700 && scale <= 1.0) ""
+        else if (x <= 1700) "Press up or down to zoom in/out."
+        else if (x <= 1800) "How do other stars compare to the size of the Sun? Keep going right to find out."
         else if (x <= 2000) ""
+        else if (x <= 2250) "This is Sirius A, the brightest star in the night sky."
+        else if (x <= 2600) ""
+        else if (x <= 3000) "Get ready for something big"
+        else if (x <= 3200) "This is betelgeuse, one of the largest stars seen from earth."
         else ""
     }
 
@@ -38,9 +42,10 @@ object UniverseVisualizer extends JFXApp {
 
             val earth = new SpaceObject(Color.Blue, 640, 0.009)
             val sun = new SpaceObject(Color.White, 1500, 1.0)
-            val betelgeuse = new SpaceObject(Color.Red, 3000, 764.0)
+            val sirius = new SpaceObject(Color.LightBlue, 2500, 1.711)
+            val betelgeuse = new SpaceObject(Color.Red, 3500, 764.0)
 
-            val objects = Vector(sun, earth, betelgeuse)
+            val objects = Vector(sun, earth, sirius, betelgeuse)
             var screenX = 640L
 
             var oldT = 0L
@@ -55,7 +60,7 @@ object UniverseVisualizer extends JFXApp {
                     objects.foreach(_.draw(g, scale))
 
                     g.setFill(Color.White)
-                    g.fillText(caption(screenX, scale), 100, displayHeight - 100)
+                    g.fillText(caption(screenX), 100, displayHeight - 100)
 
                     if ((keysPressed.contains(KeyCode.Down) || keysPressed.contains(KeyCode.S)) && scale > 1.0) scale -= 1.0
                     if ((keysPressed.contains(KeyCode.Up) || keysPressed.contains(KeyCode.W))) scale += 1.0
