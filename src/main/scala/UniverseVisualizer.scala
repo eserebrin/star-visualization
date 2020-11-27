9aa187094058925e364d838d1fbcb861ed92e3fd
Ezra Serebrin <ezraserebrin@gmail.com>
2020-11-26 22:18:26 -0600

    Captions

--- src/main/scala/UniverseVisualizer.scala
+++ src/main/scala/UniverseVisualizer.scala
@@ -12,6 +12,16 @@ object UniverseVisualizer extends JFXApp {
     val displayWidth = 1280
     val displayHeight = 720
 
+    def caption(x: Long, scale: Double): String = {
+        if (x <= 1000) "This is Earth. Press right to see the Sun"
+        else if (x <= 1150) "The sun is huge in comparison to the small blue dot that is our home."
+        else if (x <= 1500) ""
+        else if (x <= 1700 && scale > 1.0) "Press down to zoom out and see the Sun as just one pixel."
+        else if (x <= 1700 && scale <= 1.0) ""
+        else if (x <= 2000) ""
+        else ""
+    }
+
     stage = new JFXApp.PrimaryStage {
         title = "If the Sun was 1 pixel"
         scene = new Scene(displayWidth, displayHeight) {
@@ -24,32 +34,40 @@ object UniverseVisualizer extends JFXApp {
             canvas.onKeyPressed = (e: KeyEvent) => keysPressed += e.code
             canvas.onKeyReleased = (e: KeyEvent) => keysPressed -= e.code
 
-            var scale = 109.0
+            var scale = 250.0
 
             val earth = new SpaceObject(Color.Blue, 640, 0.009)
             val sun = new SpaceObject(Color.White, 1500, 1.0)
             val betelgeuse = new SpaceObject(Color.Red, 3000, 764.0)
 
             val objects = Vector(sun, earth, betelgeuse)
-            val captions = Vector(("This is Earth", 0, 500))
+            var screenX = 640L
 
             var oldT = 0L
             var timer = 0L
             val mainLoop = AnimationTimer(t => {
                 if (t - oldT >= 1e9 / 60) {
+                    println(s"Scale = $scale, X = $screenX")
+
                     g.setFill(Color.Black)
                     g.fillRect(0, 0, displayWidth, displayHeight)
 
                     objects.foreach(_.draw(g, scale))
 
                     g.setFill(Color.White)
-                    captions.foreach(c => g.fillText(c._1, 100, displayHeight - 100))
+                    g.fillText(caption(screenX, scale), 100, displayHeight - 100)
 
-                    if (keysPressed.contains(KeyCode.Down) && scale > 1.0) scale -= 1.0
-                    if (keysPressed.contains(KeyCode.Up)) scale += 1.0
+                    if ((keysPressed.contains(KeyCode.Down) || keysPressed.contains(KeyCode.S)) && scale > 1.0) scale -= 1.0
+                    if ((keysPressed.contains(KeyCode.Up) || keysPressed.contains(KeyCode.W))) scale += 1.0
 
-                    if (keysPressed.contains(KeyCode.Right)) objects.foreach(_.moveRight())
-                    if (keysPressed.contains(KeyCode.Left)) objects.foreach(_.moveLeft())
+                    if (keysPressed.contains(KeyCode.Right) || keysPressed.contains(KeyCode.D)) {
+                        objects.foreach(_.moveRight())
+                        screenX += 1
+                    }
+                    if (keysPressed.contains(KeyCode.Left) || keysPressed.contains(KeyCode.A)) {
+                        objects.foreach(_.moveLeft())
+                        screenX -= 1
+                    }
 
                     oldT = t
                 }
